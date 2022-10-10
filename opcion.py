6f6275aa56877ee6b1633115c4992b31e4a262db
xAEvi <105865102+xAEvi@users.noreply.github.com>
2022-10-10 10:38:24 -0500

    Update opcion.py

--- opcion.py
+++ opcion.py
@@ -1,24 +1,23 @@
+from sqlite3 import Row
 import tkinter
-from tkinter import IntVar, ttk
+from tkinter import IntVar, StringVar, ttk
 
 ventana = tkinter.Tk()
 
 seleccionado = tkinter.StringVar()
 
 def mostrar():
-    print(f'Opción: {str(radio.get())}')
+    texto.config(text={str(radio.get())})
 
-radio = IntVar()
+radio = StringVar()
 
 r1 = ttk.Radiobutton(ventana, text="Opción 1", value="1", variable=radio, command=mostrar)
 r2 = ttk.Radiobutton(ventana, text="Opción 2", value="2", variable=radio, command=mostrar)
 r3 = ttk.Radiobutton(ventana, text="Opción 3", value="3", variable=radio, command=mostrar)
-
+texto = tkinter.Label(ventana, text="hola")
 
 def limpiar():
-    r1.selection_clear()
-    r2.selection_clear()
-    r3.selection_clear()
+    radio.set(None)
 
 boton = tkinter.Button(ventana, text="Reiniciar", command=limpiar)
 
@@ -26,6 +25,7 @@ r1.grid(column=0,row=0)
 r2.grid(column=0,row=1)
 r3.grid(column=0,row=2)
 boton.grid(column=0,row=3)
+texto.grid(column=0,row=4)
 
 
 ventana.mainloop()
